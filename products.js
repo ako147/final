ba2942e9f37407e8e9bae8aa529ac71f0340f89e
etoshi05 <ting94118@gmail.com>
2026-01-03 16:07:03 +0800

    修正

--- products.js
+++ products.js
@@ -22,21 +22,21 @@ function initFavoriteButtons() {
 
 // ===== 商品資料 =====
 const products = [
-    { id:101, name:"2026 潮流百搭厚邊框鈦輕盈系列", price:3980, colors:["gold","black"], category:"UNISEX", material:"titanium", shape:"other", type:"光學眼鏡", url:"goods/good1.html", image:"image/goods/1/1-1.jpg" },
+    { id:101, name:"2026 潮流百搭厚邊框鈦輕盈系列", price:3980, colors:["gold","gray"], category:"UNISEX", material:"titanium", shape:"other", type:"光學眼鏡", url:"goods/good1.html", image:"image/goods/1/1-1.jpg" },
     { id:102, name:"2026 潮流百搭厚邊框鈦輕盈系列", price:3980, colors:["gold","gray"], category:"UNISEX", material:"titanium", shape:"wellington", type:"光學眼鏡", url:"goods/good2.html", image:"image/goods/2/1-1.jpg" },
     { id:103, name:"2026 潮流百搭厚邊框鈦輕盈系列", price:3980, colors:["black"], category:"UNISEX", material:"titanium", shape:"square", type:"光學眼鏡", url:"goods/good3.html", image:"image/goods/3/1-1.jpg" },
-    { id:104, name:"經典時尚系列 墨鏡", price:3980, colors:["black"], category:"UNISEX", material:"塑膠", shape:"無", type:"太陽眼鏡", url:"goods/good4.html", image:"image/goods/4/1-1.jpg" },
+    { id:104, name:"經典時尚系列 墨鏡", price:3980, colors:["black"], category:"UNISEX", material:"plastic", shape:"", type:"太陽眼鏡", url:"goods/good4.html", image:"image/goods/4/1-1.jpg" },
     { id:105, name:"金屬濾藍光眼鏡", price:1584, colors:["gold","pink"], category:"UNISEX", material:"metal", shape:"postom", type:"抗藍光眼鏡", url:"goods/good5.html", image:"image/goods/5/1-1.jpg" },
-    { id:106, name:"2026 潮流百搭厚邊框鈦輕盈系列", price:3980, colors:["gold","black"], category:"UNISEX", material:"titanium", shape:"other", type:"光學眼鏡", url:"goods/good6.html", image:"image/goods/6/1-1.jpg" },
-    { id:107, name:"2026 潮流百搭厚邊框鈦輕盈系列", price:3980, colors:["gold","gray"], category:"UNISEX", material:"titanium", shape:"wellington", type:"光學眼鏡", url:"goods/good7.html", image:"image/goods/7/1-1.jpg" },
-    { id:108, name:"2026 潮流百搭厚邊框鈦輕盈系列", price:3980, colors:["black"], category:"UNISEX", material:"titanium", shape:"square", type:"光學眼鏡", url:"goods/good8.html", image:"image/goods/8/1-1.jpg" },
-    { id:109, name:"經典時尚系列 墨鏡", price:3980, colors:["black"], category:"UNISEX", material:"plastic", shape:"none", type:"太陽眼鏡", url:"goods/good9.html", image:"image/goods/9/1-1.jpg" },
-    { id:110, name:"金屬濾藍光眼鏡", price:1584, colors:["gold","pink"], category:"UNISEX", material:"metal", shape:"postom", type:"抗藍光眼鏡", url:"goods/good10.html", image:"image/goods/10/1-1.jpg" },
-    { id:111, name:"2026 潮流百搭厚邊框鈦輕盈系列", price:3980, colors:["gold","black"], category:"UNISEX", material:"titanium", shape:"other", type:"光學眼鏡", url:"goods/good11.html", image:"image/goods/11/1-1.jpg" },
-    { id:112, name:"2026 潮流百搭厚邊框鈦輕盈系列", price:3980, colors:["gold","gray"], category:"UNISEX", material:"titanium", shape:"wellington", type:"光學眼鏡", url:"goods/good12.html", image:"image/goods/12/1-1.jpg" },
-    { id:113, name:"2026 潮流百搭厚邊框鈦輕盈系列", price:3980, colors:["black"], category:"UNISEX", material:"titanium", shape:"square", type:"光學眼鏡", url:"goods/good13.html", image:"image/goods/13/1-1.jpg" },
-    { id:114, name:"經典時尚系列 墨鏡", price:3980, colors:["black"], category:"UNISEX", material:"plastic", shape:"none", type:"太陽眼鏡", url:"goods/good14.html", image:"image/goods/14/1-1.jpg" },
-    { id:115, name:"金屬濾藍光眼鏡", price:1584, colors:["gold","pink"], category:"UNISEX", material:"metal", shape:"postom", type:"抗藍光眼鏡", url:"goods/good15.html", image:"image/goods/15/1-1.png" }
+    { id:106, name:"無度數濾藍光盒裝眼鏡", price:1584, colors:["brown"], category:"UNISEX", material:"metal", shape:"postom", type:"抗藍光眼鏡", url:"goods/good6.html", image:"image/goods/6/1-1.jpg" },
+    { id:107, name:"Chill墨鏡系列", price:1736, colors:["black","gray"], category:"WOMENS", material:"metal", shape:"other", type:"太陽眼鏡", url:"goods/good7.html", image:"image/goods/7/1-1.jpg" },
+    { id:108, name:"經典時尚系列", price:4980, colors:["brown"], category:"WOMENS", material:"plastic", shape:"square", type:"光學眼鏡", url:"goods/good8.html", image:"image/goods/8/1-1.jpg" },
+    { id:109, name:"360°®超彈力耐壓系列 兒童款", price:2980, colors:["pink","brown","blue"], category:"KID&JUNIOR", material:"resin", shape:"oval", type:"光學眼鏡", url:"goods/good9.html", image:"image/goods/9/1-1.jpg" },
+    { id:110, name:"2024 日本製春夏系列眼鏡", price:3980, colors:["green","pink","clear"], category:"UNISEX", material:"resin", shape:"", type:"光學眼鏡", url:"goods/good10.html", image:"image/goods/10/1-1.jpg" },
+    { id:111, name:"復古厚感膠框系列", price:1899, colors:["pink","gray","brown"], category:"UNISEX", material:"resin", shape:"square", type:"光學眼鏡", url:"goods/good11.html", image:"image/goods/11/1-1.jpg" },
+    { id:112, name:"Switch 2024 SWITCH Renew系列", price:2786, colors:["black"], category:"MENS", material:"resin", shape:"wellington", type:"光學眼鏡", url:"goods/good12.html", image:"image/goods/12/1-1.jpg" },
+    { id:113, name:"Chill系列", price:2533, colors:["black","gray"], category:"MENS", material:"resin", shape:"square", type:"光學眼鏡", url:"goods/good13.html", image:"image/goods/13/1-1.jpg" },
+    { id:114, name:"STAR WARS設計款 角色系列-孩童款", price:2786, colors:["brown"], category:"KID&JUNIOR", material:"resin", shape:"wellington", type:"太陽眼鏡", url:"goods/good14.html", image:"image/goods/14/1-1.jpg" },
+    { id:115, name:"ZOOTOPIA 眼鏡盒", price:390, colors:["green","gray"], category:"CASE", material:"", shape:"", type:"配件", url:"goods/good15.html", image:"image/goods/15/1-1.png" }
 ];
 
 // ===== DOM 元素 =====
@@ -69,14 +69,21 @@ function renderProducts(list){
             grid.innerHTML += `
                 <a href="${p.url}" class="product-link">
                   <article class="product-card">
-                    <button class="favorite ${isFav ? "active" : ""}" data-id="${p.id}">♥</button>
+                    <button class="favorite ${isFav ? "active" : ""}" data-id="${p.id}">
+                        ${isFav ? "♥" : "♡"}
+                    </button>
+                    
+
 
                     <div class="product-image">
                       <img src="${p.image}" alt="${p.name}">
                     </div>
-
+                    
                     <p class="title">${p.name}</p>
                     <p class="price">NT$${p.price}</p>
+                    <div class="colors">
+                        ${p.colors.map(c => `<span class="color ${c}"></span>`).join("")}
+                    </div>
                   </article>
                 </a>
                 `;
